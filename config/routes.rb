239aa4637ceb9e8bd66389500daec136fcda92f5
developer <shpandey@bestpeers.com>
2023-05-19 16:41:03 +0530

    BLOGAPP

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+# Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+    # root "articles#index"
+
+    # resources :articles
+
+
+# end
+###comment
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
